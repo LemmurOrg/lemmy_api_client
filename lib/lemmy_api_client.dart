3f5228379fb21e4d2205d9fab95bff9a3927c780
shilangyu <xmarcinmarcin@gmail.com>
2020-08-21 00:10:28 +0000

    added source files

---
+++ lib/lemmy_api_client.dart
@@ -0,0 +1,15 @@
+import 'src/v1/main.dart';
+
+export 'src/v1/main.dart';
+
+class LemmyAPI {
+  /// host uri of this lemmy instance
+  String host;
+
+  V1 v1;
+
+  /// initialize lemmy api instance
+  LemmyAPI(this.host)
+      : assert(host != null),
+        v1 = V1(host);
+}
