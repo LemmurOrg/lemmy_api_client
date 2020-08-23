e9437b4f6a6f8109622b250767102efe820e466d
shilangyu <xmarcinmarcin@gmail.com>
2020-08-23 21:11:43 +0000

    better exception messages

--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -6,11 +6,16 @@ class InvalidAuthException implements Exception {
   InvalidAuthException(this._message);
 
   @override
-  String toString() => _message;
+  String toString() => '${runtimeType.toString()}: $_message';
 }
 
 class UnknownResponseException implements Exception {
   final http.Response res;
 
   UnknownResponseException(this.res);
+
+  @override
+  String toString() =>
+      '${runtimeType.toString()}: request failed for an unknown reason.'
+      ' This should not have been thrown, please open an issue: https://github.com/krawieck/lemmy_api_client';
 }
